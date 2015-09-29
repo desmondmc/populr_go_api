4631f11834d54f912f61d03286a741e664a52d60
Desmond McNamee <desmondmcnamee@gmail.com>
2015-09-29 23:42:22 +0100

    Ran godep

--- handlers_user.go
+++ handlers_user.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/desmondmcnamee/populr_go_api/Godeps/_workspace/src/github.com/nu7hatch/gouuid"
 	"github.com/desmondmcnamee/populr_go_api/Godeps/_workspace/src/golang.org/x/crypto/bcrypt"
-	"github.com/nu7hatch/gouuid"
 
 	"github.com/desmondmcnamee/populr_go_api/Godeps/_workspace/src/github.com/gorilla/context"
 	"github.com/desmondmcnamee/populr_go_api/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
